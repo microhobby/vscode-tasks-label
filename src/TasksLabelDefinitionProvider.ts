e9a9759c5b13fdc654874cf4f98ae7661e09e728
Leonardo Bornia <leonardo.bornia@toradex.com>
2025-05-23 15:37:44 -0300

    vscode-tasks-label: TasksLabelDefinitionProvider: Add multiroot support
    Related-to: TIE-914
    Signed-off-by: Leonardo Bornia leonardo.bornia@toradex.com

--- src/TasksLabelDefinitionProvider.ts
+++ src/TasksLabelDefinitionProvider.ts
@@ -201,56 +201,69 @@ export class TasksLabelDefinitionProvider implements
 	}
 
 	private _isIncludeFile(fileName: string): boolean {
-		const _includeFiles =
-            vscode.workspace
-            	.getConfiguration("tasksLabel")
-            	.get("includeFiles") as string[];
+		for (const folder of vscode.workspace.workspaceFolders!) {
+			const rootPath = folder.uri.fsPath;
+			const config = vscode.workspace.getConfiguration(
+				"tasksLabel", folder.uri
+			);
+			const includeFiles = config.get("includeFiles") as string[] ?? [];
 
-		_includeFiles.push(".vscode/launch.json");
-		_includeFiles.push(".vscode/tasks.json");
+			let fileList = [
+				".vscode/tasks.json",
+				".vscode/launch.json",
+				...includeFiles
+			].map(relativePath => Path.join(rootPath, relativePath));
+			// launch.json may not exist in multiroot workspace
+			fileList = fileList.filter(fs.existsSync);
 
-		for (let index = 0; index < _includeFiles.length; index++) {
-			const includeFile = _includeFiles[index];
-			if (fileName.endsWith(includeFile)) {
-				return true;
+			for (let index = 0; index < fileList.length; index++) {
+				const includeFile = fileList[index];
+				if (fileName.endsWith(includeFile)) {
+					return true;
+				}
 			}
 		}
-
 		return false;
 	}
 
 	public getDefinedTasks (): Task[] {
-		const fileList = [
-			".vscode/tasks.json",
-			".vscode/launch.json",
-			...vscode
-				.workspace
-				.getConfiguration("tasksLabel")
-				.get("includeFiles") as string[]
-		];
-
-		// for each file get the label name and store it
 		const labels = Array<Task>();
-		for (const file of fileList) {
-			try {
-				const path = Path.join(vscode.workspace.rootPath!, file);
-				const text = fs.readFileSync(path, 'utf8');
-				const lines = text.split(/\r?\n/g);
+		for (const folder of vscode.workspace.workspaceFolders!) {
+			const rootPath = folder.uri.fsPath;
+			const config = vscode.workspace.getConfiguration(
+				"tasksLabel", folder.uri
+			);
+			const includeFiles = config.get("includeFiles") as string[] ?? [];
 
-				let lineNum = 0;
-				for (const line of lines) {
-					const match = line.match(/"label":\s*"(.*)"/);
-					if (match) {
-						labels.push({
-							label: match[1],
-							line: lineNum
-						});
+			let fileList = [
+				".vscode/tasks.json",
+				".vscode/launch.json",
+				...includeFiles
+			].map(relativePath => Path.join(rootPath, relativePath));
+			// launch.json may not exist in multiroot workspace
+			fileList = fileList.filter(fs.existsSync);
+
+			for (const file of fileList) {
+				try {
+					const text = fs.readFileSync(file, 'utf8');
+					const lines = text.split(/\r?\n/g);
+
+					let lineNum = 0;
+					// for each file get the label name and store it
+					for (const line of lines) {
+						const match = line.match(/"label":\s*"(.*)"/);
+						if (match) {
+							labels.push({
+								label: match[1],
+								line: lineNum
+							});
+						}
+
+						lineNum++;
 					}
-
-					lineNum++;
+				} catch (ex: unknown) {
+					console.log(ex);
 				}
-			} catch (ex: unknown) {
-				console.log(ex);
 			}
 		}
 
@@ -258,51 +271,75 @@ export class TasksLabelDefinitionProvider implements
 	}
 
 	private async _parseTasks (): Promise<void> {
-		const fileList = [
-			".vscode/tasks.json",
-			".vscode/launch.json",
-			...vscode
-				.workspace
-				.getConfiguration("tasksLabel")
-				.get("includeFiles") as string[]
-		];
-
-		// for each file get the label name and store it
 		const labels = Array<Task>();
-		for (const file of fileList) {
-			const path = Path.join(vscode.workspace.rootPath!, file);
-			const fileUri = vscode.Uri.file(path);
-			const document = await vscode.workspace.openTextDocument(fileUri);
-			const scanner = JSONC.createScanner(document.getText());
 
-			let tokenCode = 0;
-			while (tokenCode !== JSONC.SyntaxKind.EOF) {
-				tokenCode = scanner.scan();
+		for (const folder of vscode.workspace.workspaceFolders!) {
+			const rootPath = folder.uri.fsPath;
+			const config = vscode.workspace.getConfiguration(
+				"tasksLabel", folder.uri
+			);
+			const includeFiles = config.get("includeFiles") as string[] ?? [];
 
-				// is this a label?
-				if (
-					tokenCode === JSONC.SyntaxKind.StringLiteral &&
-					scanner.getTokenValue() === "label"
-				) {
-					// get the label name
+			let fileList = [
+				".vscode/tasks.json",
+				".vscode/launch.json",
+				...includeFiles
+			].map(relativePath => Path.join(rootPath, relativePath));
+			// launch.json may not exist in multiroot workspace
+			fileList = fileList.filter(fs.existsSync);
+
+			for (const file of fileList) {
+				const fileUri = vscode.Uri.file(file);
+				const document = await vscode.workspace.openTextDocument(
+					fileUri
+				);
+				const scanner = JSONC.createScanner(document.getText());
+
+				let tokenCode = 0;
+				while (tokenCode !== JSONC.SyntaxKind.EOF) {
 					tokenCode = scanner.scan();
 
-					while (tokenCode !== JSONC.SyntaxKind.EOF) {
-						if (tokenCode === JSONC.SyntaxKind.StringLiteral) {
-							// get the label value
-							const labelValue = scanner.getTokenValue();
+					// is this a label?
+					if (
+						tokenCode === JSONC.SyntaxKind.StringLiteral &&
+						scanner.getTokenValue() === "label"
+					) {
+						// get the label name
+						tokenCode = scanner.scan();
 
-							// update or create?
-							const labelRef = labels.findIndex(
-								obj => obj.label === labelValue
-							);
+						while (tokenCode !== JSONC.SyntaxKind.EOF) {
+							if (tokenCode === JSONC.SyntaxKind.StringLiteral) {
+								// get the label value
+								const labelValue = scanner.getTokenValue();
 
-							if (labelRef === -1) {
-								// create
-								labels.push({
-									label: labelValue,
-									line: scanner.getTokenOffset(),
-									location: new vscode.Location(
+								// update or create?
+								const labelRef = labels.findIndex(
+									obj => obj.label === labelValue
+								);
+
+								if (labelRef === -1) {
+									// create
+									labels.push({
+										label: labelValue,
+										line: scanner.getTokenOffset(),
+										location: new vscode.Location(
+											fileUri,
+											new vscode.Range(
+												document.positionAt(
+													scanner.getTokenOffset()
+												),
+												document.positionAt(
+													scanner.getTokenOffset()
+													+
+													scanner.getTokenLength()
+												)
+											)
+										)
+									});
+								} else {
+									// update
+									labels[labelRef].location =
+									new vscode.Location(
 										fileUri,
 										new vscode.Range(
 											document.positionAt(
@@ -314,161 +351,155 @@ export class TasksLabelDefinitionProvider implements
 												scanner.getTokenLength()
 											)
 										)
-									)
-								});
-							} else {
-								// update
-								labels[labelRef].location = new vscode.Location(
-									fileUri,
-									new vscode.Range(
-										document.positionAt(
-											scanner.getTokenOffset()
-										),
-										document.positionAt(
-											scanner.getTokenOffset()
-											+
-											scanner.getTokenLength()
-										)
-									)
-								);
-								labels[labelRef].line =
-									scanner.getTokenOffset();
-							}
-
-							break;
-						}
-
-						tokenCode = scanner.scan();
-					}
-				} else if (
-					tokenCode === JSONC.SyntaxKind.StringLiteral &&
-					scanner.getTokenValue() === "dependsOn"
-				) {
-					// get the dependsOn value
-					do {
-						tokenCode = scanner.scan();
-
-						if (tokenCode === JSONC.SyntaxKind.StringLiteral) {
-							// get the dependsOn value
-							const dependsOnValue = scanner.getTokenValue();
-
-							// find the label reference
-							let labelRef = labels.findIndex(
-								obj => obj.label === dependsOnValue
-							);
-
-							// is possible that the definitions was not located
-							if (labelRef === -1) {
-							 	labelRef = labels.push(
-									{
-										label: dependsOnValue,
-										line: scanner.getTokenOffset()
-									}
-								) -1;
-							}
-
-							if (labelRef > 0) {
-								if (labels[labelRef].references === undefined) {
-									labels[labelRef].references = [];
+									);
+									labels[labelRef].line =
+										scanner.getTokenOffset();
 								}
 
-								labels[labelRef].references!.push(
-									{
-										range: new vscode.Range(
-											document.positionAt(
-												scanner.getTokenOffset()
-											),
-											document.positionAt(
-												scanner.getTokenOffset()
-													+
-												scanner.getTokenLength()
-											)
-										),
-										uri: fileUri
-									}
-								);
+								break;
 							}
+
+							tokenCode = scanner.scan();
 						}
-					} while (
-						tokenCode !== JSONC.SyntaxKind.CloseBracketToken &&
-							tokenCode !== JSONC.SyntaxKind.EOF
-					);
-				}
-			}
-		}
+					} else if (
+						tokenCode === JSONC.SyntaxKind.StringLiteral &&
+						scanner.getTokenValue() === "dependsOn"
+					) {
+						// get the dependsOn value
+						do {
+							tokenCode = scanner.scan();
 
-		this.references = labels;
-		this._onDidChangeCodeLenses.fire();
-	}
+							if (tokenCode === JSONC.SyntaxKind.StringLiteral) {
+								// get the dependsOn value
+								const dependsOnValue = scanner.getTokenValue();
 
-	private _checkIfDependsOnAreDefined (document: vscode.TextDocument): void {
-		const diagnosticEnabled =
-			vscode
-				.workspace
-				.getConfiguration("tasksLabel")
-				.get("diagnostics") as boolean;
+								// find the label reference
+								let labelRef = labels.findIndex(
+									obj => obj.label === dependsOnValue
+								);
 
-		if (diagnosticEnabled) {
-			const jsonObj = JSONC.parse(document.getText());
-			const _diagnostics: vscode.Diagnostic[] = [];
+								// if definitions were not located
+								if (labelRef === -1) {
+									labelRef = labels.push(
+										{
+											label: dependsOnValue,
+											line: scanner.getTokenOffset()
+										}
+									) -1;
+								}
 
-			// for each task that has dependsOn check the list of labels
-			const tasks = jsonObj.tasks ?? [];
+								if (labelRef > 0) {
+									if (
+										labels[
+											labelRef
+										].references === undefined
+									) {
+										labels[labelRef].references = [];
+									}
 
-			for (const task of tasks) {
-				if (task.dependsOn !== undefined && task.dependsOn.length > 0) {
-					for (const label of task.dependsOn) {
-						const labels = this.getDefinedTasks().map(
-							obj => obj.label
-						);
-
-						if (label) {
-							// so now we have work to do
-							// find out the line where this label is
-							const scanner =
-								JSONC.createScanner(document.getText());
-
-							// scann the document for all the dependsOn
-							let tokenCode = 0;
-							while (tokenCode !== JSONC.SyntaxKind.EOF) {
-								tokenCode = scanner.scan();
-
-								if (
-									tokenCode ===
-										JSONC.SyntaxKind.StringLiteral &&
-									scanner.getTokenValue() === label
-								) {
-									if (!labels.includes(label)) {
-										// add it to the diagnostics
-										_diagnostics.push({
-											code: '',
-											// eslint-disable-next-line max-len
-											message: `Task "${label}" is not defined`,
+									labels[labelRef].references!.push(
+										{
 											range: new vscode.Range(
 												document.positionAt(
 													scanner.getTokenOffset()
 												),
 												document.positionAt(
-													scanner.getTokenOffset() +
+													scanner.getTokenOffset()
+														+
 													scanner.getTokenLength()
 												)
 											),
-											severity:
-												vscode.DiagnosticSeverity.Error,
-											source: 'tasksLabel'
-										});
+											uri: fileUri
+										}
+									);
+								}
+							}
+						} while (
+							tokenCode !== JSONC.SyntaxKind.CloseBracketToken &&
+								tokenCode !== JSONC.SyntaxKind.EOF
+						);
+					}
+				}
+			}
+		}
+		this.references = labels;
+		this._onDidChangeCodeLenses.fire();
+	}
+
+	private _checkIfDependsOnAreDefined (document: vscode.TextDocument): void {
+		for (const folder of vscode.workspace.workspaceFolders!) {
+			const config = vscode.workspace.getConfiguration(
+				"tasksLabel", folder.uri
+			);
+			const diagnosticEnabled = config.get(
+				"diagnostics"
+			) as boolean ?? false;
+
+			if (diagnosticEnabled) {
+				const jsonObj = JSONC.parse(document.getText());
+				const _diagnostics: vscode.Diagnostic[] = [];
+
+				// for each task that has dependsOn check the list of labels
+				const tasks = jsonObj.tasks ?? [];
+
+				for (const task of tasks) {
+					if (
+						task.dependsOn !== undefined
+						&& task.dependsOn.length > 0
+					) {
+						for (const label of task.dependsOn) {
+							const labels = this.getDefinedTasks().map(
+								obj => obj.label
+							);
+
+							if (label) {
+								// so now we have work to do
+								// find out the line where this label is
+								const scanner =
+									JSONC.createScanner(document.getText());
+
+								// scann the document for all the dependsOn
+								let tokenCode = 0;
+								while (tokenCode !== JSONC.SyntaxKind.EOF) {
+									tokenCode = scanner.scan();
+
+									if (
+										tokenCode ===
+											JSONC.SyntaxKind.StringLiteral &&
+										scanner.getTokenValue() === label
+									) {
+										if (!labels.includes(label)) {
+											// add it to the diagnostics
+											_diagnostics.push({
+												code: '',
+												// eslint-disable-next-line max-len
+												message: `Task "${label}" is not defined`,
+												range: new vscode.Range(
+													document.positionAt(
+														scanner.getTokenOffset()
+													),
+													document.positionAt(
+														scanner.getTokenOffset() +
+														scanner.getTokenLength()
+													)
+												),
+												severity:
+													vscode.DiagnosticSeverity.Error,
+												source: 'tasksLabel'
+											});
+										}
 									}
 								}
 							}
 						}
 					}
 				}
-			}
 
-			// done, set it
-			this.diagnostics.set(document.uri, _diagnostics);
-		} else {
-			this.diagnostics.clear();
+				// done, set it
+				this.diagnostics.set(document.uri, _diagnostics);
+			} else {
+				this.diagnostics.clear();
+			}
 		}
 	}
 
@@ -508,35 +539,34 @@ export class TasksLabelDefinitionProvider implements
 			word = word.replace(/"/g, '');
 
 			// pass trough the files
-			const files = Array<{
-                path: string,
-                content: string
-            }>();
-			const _includeFiles =
-                vscode
-                	.workspace
-                	.getConfiguration("tasksLabel")
-                	.get("includeFiles") as string[];
-			_includeFiles.push(".vscode/tasks.json");
+			const files: { path: string, content: string }[] = [];
+			const baseIncludeFiles = vscode.workspace.getConfiguration(
+				"tasksLabel").get("includeFiles"
+			) as string[] ?? [];
+			const includeFiles = [".vscode/tasks.json", ...baseIncludeFiles];
 
-			// read the files
-			for (let index = 0; index < _includeFiles.length; index++) {
-				try {
-					const includeFile = _includeFiles[index];
-					// if (document.fileName.endsWith(includeFile)) {
-					// 	continue;
-					// }
+			for (const folder of vscode.workspace.workspaceFolders ?? []) {
+				const rootPath = folder.uri.fsPath;
+				const config = vscode.workspace.getConfiguration(
+					"tasksLabel", folder.uri
+				);
+				const folderIncludeFiles = config.get(
+					"includeFiles"
+				) as string[] ?? includeFiles;
 
-					// FIXME: this will not work for multi-root workspaces
-					path = Path.join(vscode.workspace.rootPath!, includeFile);
-					text = fs.readFileSync(path, 'utf8');
+				const resolvedFiles = [
+					".vscode/tasks.json",
+					...folderIncludeFiles
+				]
+					.map(rel => Path.join(rootPath, rel));
 
-					files.push({
-						"path": path,
-						"content": text
-					});
-				} catch (ex: unknown) {
-					console.log(ex);
+				for (const file of resolvedFiles) {
+					try {
+						const content = fs.readFileSync(file, 'utf8');
+						files.push({ path: file, content });
+					} catch (ex: unknown) {
+						console.log(`Error reading ${file}`, ex);
+					}
 				}
 			}
 
